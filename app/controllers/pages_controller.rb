7b39b993ac72356e96ed2e3bd2369c4c8969091a
QGarden <qjgarden1@gmail.com>
2022-04-30 02:17:21 -0400

    3.1 completed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
+    before_action :set_page, only: [:show,:edit,:update,:destroy]
     def index
         @pages=Page.all
     end
     def show
-        @page=Page.find(params[:id])
+        
     #    render plain:@page.title
     end
     def new
@@ -17,5 +18,38 @@ class PagesController < ApplicationController
         @page.save
         redirect_to @page
     end
+
+    def edit
+        
+
+    end
+    def update
+        
+    
+        @page.update(page_params)
+        redirect_to @page
+        
+    end
+
+    def destroy
+    
+        @page.destroy
+        redirect_to pages_path
+    end
+
+
+    private
+        def page_params
+            page_params=params.require(:page).permit(:title,:body,:slug)
+
+        end
+
+        def set_page
+            @page= @page=Page.find(params[:id])
+        end
+        
+
+      
+
   
 end
